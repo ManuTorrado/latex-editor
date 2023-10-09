dba683c3bb883467069e8963a9851ec61bb1c13c
ManuTorrado <manuel.torrado@gmail.com>
2023-10-09 20:08:42 -0300

    Creates component's folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import React, { useState } from 'react';
-import './App.css';
-import Display from './Display';
-import LatexInput from './LatexInput';
+import React, { useState } from "react";
+import "./App.css";
+import Display from "./comps/Display";
+import LatexInput from "./comps/LatexInput";
 
 function App() {
-  const [areaInput, setAreaInput] = useState<string>('');
+  const [areaInput, setAreaInput] = useState<string>("");
 
-  const handleInput = (event: React.FormEvent<HTMLDivElement>) => setAreaInput(event.currentTarget.textContent? event.currentTarget.textContent : '')
+  const handleInput = (event: React.FormEvent<HTMLDivElement>) =>
+    setAreaInput(
+      event.currentTarget.textContent ? event.currentTarget.textContent : ""
+    );
 
   return (
-    <div style={{display: 'flex'}}>
-      <LatexInput onInput={handleInput}/>
-     <Display content={areaInput}/>
+    <div style={{ display: "flex" }}>
+      <LatexInput onInput={handleInput} />
+      <Display content={areaInput} />
     </div>
   );
 }
