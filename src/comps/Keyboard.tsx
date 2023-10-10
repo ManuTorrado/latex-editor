bd33cd278b24d50a51e332fdcc98f3a7010ada0d
ManuTorrado <manuel.torrado@gmail.com>
2023-10-09 23:33:02 -0300

    Starts implementing a tab system

--- src/comps/Keyboard.tsx
+++ src/comps/Keyboard.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Latex from "react-latex";
 import { symbols, letters } from "../latex-symbols";
-import Tab from "./Tab";
-import Tabs from "./Tabs";
+import Tab from "./Tab/Tab";
+import Tabs from "./Tab/Tabs";
+import TabProvider from "./Tab/TabContext";
 
 const Keyboard = () => {
   const buttonsSize: string = "50px";
@@ -17,24 +18,28 @@ const Keyboard = () => {
       }}
     >
       <div>
-        <Tabs>
-          <Tab label="Letters">
-            {letters.map((letter) => (
-              <button
-                style={{
-                  maxHeight: buttonsSize,
-                  minHeight: buttonsSize,
-                  maxWidth: buttonsSize,
-                  minWidth: buttonsSize,
-                }}
-              >
-                {" "}
-                <Latex displayMode={true}>{"$$" + letter.comando + "$$"}</Latex>
-              </button>
-            ))}
-          </Tab>
-          <Tab label="Simbols"></Tab>
-        </Tabs>
+        <TabProvider>
+          <Tabs>
+            <Tab label="Letters">
+              {letters.map((letter) => (
+                <button
+                  style={{
+                    maxHeight: buttonsSize,
+                    minHeight: buttonsSize,
+                    maxWidth: buttonsSize,
+                    minWidth: buttonsSize,
+                  }}
+                >
+                  {" "}
+                  <Latex displayMode={true}>
+                    {"$$" + letter.comando + "$$"}
+                  </Latex>
+                </button>
+              ))}
+            </Tab>
+            <Tab label="Simbols"></Tab>
+          </Tabs>
+        </TabProvider>
         {symbols.map((symbol) => (
           <button style={{ maxHeight: "70px", minHeight: "70px" }}>
             {" "}
--- src/comps/Tab/Tab.tsx
+++ src/comps/Tab/Tab.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+
+import { useTabContext } from "./TabContext";
+
 interface Props {
   children?: React.ReactNode;
   label?: string;
 }
 
 const Tab = (props: Props) => {
+  const context = useTabContext();
   return <>{props.children}</>;
 };
 
