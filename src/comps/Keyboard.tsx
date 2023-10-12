619b7329ba16c6ba86a99e07cd5d18603074ab64
ManuTorrado <manuel.torrado@gmail.com>
2023-10-12 12:42:15 -0300

    Implements tabs on keyboard

--- src/comps/Keyboard.tsx
+++ src/comps/Keyboard.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Latex from "react-latex";
 import { symbols, letters } from "../latex-symbols";
-
+import { Button } from "@chakra-ui/react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 const Keyboard = () => {
   const buttonsSize: string = "50px";
   return (
@@ -12,16 +13,40 @@ const Keyboard = () => {
         position: "fixed",
         padding: "10px",
         backgroundColor: "grey",
+        minWidth: "50%",
+        maxWidth: "50%",
+        maxHeight: "30%",
+        minHeight: "30%",
       }}
     >
-      <div>
-        {symbols.map((symbol) => (
-          <button style={{ maxHeight: "70px", minHeight: "70px" }}>
+      <Tabs variant="enclosed">
+        <TabList>
+          <Tab>
             {" "}
-            <Latex displayMode={true}>{"$$" + symbol.comando + "$$"}</Latex>
-          </button>
-        ))}
-      </div>
+            <Latex displayMode={true}>{"$$f(x)$$"}</Latex>
+          </Tab>
+          <Tab>Letters</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            {symbols.map((symbol) => (
+              <Button style={{ maxHeight: "70px", minHeight: "70px" }}>
+                {" "}
+                <Latex displayMode={true}>{"$$" + symbol.comando + "$$"}</Latex>
+              </Button>
+            ))}
+          </TabPanel>
+          <TabPanel>
+            {letters.map((symbol) => (
+              <Button style={{ maxHeight: "70px", minHeight: "70px" }}>
+                {" "}
+                <Latex displayMode={true}>{"$$" + symbol.comando + "$$"}</Latex>
+              </Button>
+            ))}
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </div>
   );
 };
